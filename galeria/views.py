fa2b2d0f4f12f65bc72887c61385b70bc149eeb5
untill99 <gabriel.kasten@outlook.com>
2024-09-01 17:44:48 -0300

    sim

--- galeria/views.py
+++ galeria/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 
 def index(request):
         return render(request, 'galeria/index.html')
+
+def imagem(request):
+        return render(request, 'galeria/imagem.html')
